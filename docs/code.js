d566adf78f648878be9cd5f6f705133fc745e1c4
khschimek <khschimek@mavs.coloradomesa.edu>
2022-09-09 13:29:55 -0600

    Hello World

--- docs/code.js
+++ docs/code.js
@@ -1,10 +1,35 @@
 number = 0;
 
-function alert() {
-    number += 1;
+function load() {
     document.getElementById("number").innerHTML = number;
 }
 
-function start() {
-    document.getElementById("number").innerHTML = number;
+function plusPointZeroZeroOne() {
+    number += 0.001;
+    load();
+}
+
+function plusPointZeroOne() {
+    number += 0.01;
+    load();
+}
+
+function plusPointOne() {
+    number += 0.1;
+    load();
+}
+
+function plusOne() {
+    number += 1;
+    load();
+}
+
+function plusTen() {
+    number += 10;
+    load();
+}
+
+function plusHundred() {
+    number += 10;
+    load();
 }
