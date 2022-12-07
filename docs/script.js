da86626f86a53b144d2a02208124b5343600711b
khsrobot <khschimek@mavs.coloradomesa.edu>
2022-12-07 01:39:03 -0700

    work

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,26 @@
+function update(day, number) {
+    if (number == -1) {
+        document.getElementById(day).innerHTML = "Not Walked";
+    }
+    else {
+        document.getElementById(day).innerHTML = "Walked";
+    }
+}
+function processing(response) {
+    if (response.op == "update") {
+        update(response.day, response.value);
+    }
+    else {
+        update(1, response.sunValue);
+        update(2, response.monValue);
+        update(3, response.tuesValue);
+        update(4, response.wednesValue);
+        update(5, response.thursValue);
+        update(6, response.friValue);
+        update(7, response.saturValue);
+    }
+}
+//---------------------------------------------------------------------------------------------
 function SendRequestToServer(url, method, request, handler) {
     const xhttp = new XMLHttpRequest();
     const encodedRequest = JSON.stringify(request);
@@ -25,19 +48,25 @@ function SendRequestToServer(url, method, request, handler) {
 
 function ProcessResponse(response) {
     if (response.status == "ok") {
-        document.getElementById("z").value = response.value;
+        processing(response);
     }
     else {
         alert(response.message);
     }
 }
 
-function addOnServer() {
-    const x=parseFloat(document.getElementById("x").value);
-    const y=parseFloat(document.getElementById("y").value);
+function updateServer(day) {
     url = "http://localhost/server.php";
     method = "POST";
-    request = {'op': 'add', 'args': [x,y]};
+    request = {'op': 'update', 'day': day};
+    SendRequestToServer(url,method,request,
+        (response)=>ProcessResponse(response));
+}
+
+function talkToServer() {
+    url = "http://localhost/server.php";
+    method = "POST";
+    request = {'op': 'talk'};
     SendRequestToServer(url,method,request,
         (response)=>ProcessResponse(response));
 }
--- docs/server.php
+++ docs/server.php
@@ -1,7 +1,8 @@
 <?php
     $request = json_decode($_REQUEST['request'], true);
-    if ($request['op']=="action") {
+    if ($request['op']=="update") {
         $ok = TRUE;
+        $action = -1.0;
         foreach ($request['args'] as $key => $value) {
             $ok = $ok && preg_match("/^(-?[0-9]+([.][0-9]+)?)$/",$value);
             if ($ok) {
@@ -10,7 +11,30 @@
         }
 
         if ($ok) {
-            $response = array('status' => 'ok', 'value' =>  $sum );
+            $response = array('status' => 'ok', 'op' => 'update', 'day' =>  $request['day'], 'value' => $action);
+        } 
+        else {
+            $response = array('status' => 'fail', 'message' => 'not all values are numbers');
+        }
+    }
+    else if ($request['op']=="talk") {
+        $ok = TRUE;
+        $sunday = -1.0;
+        $monday = -1.0;
+        $tuesday = -1.0;
+        $wednesday = -1.0;
+        $thursday = -1.0;
+        $friday = -1.0;
+        $saturday = -1.0;
+        foreach ($request['args'] as $key => $value) {
+            $ok = $ok && preg_match("/^(-?[0-9]+([.][0-9]+)?)$/",$value);
+            if ($ok) {
+                //something happens
+            }
+        }
+
+        if ($ok) {
+            $response = array('status' => 'ok',  'op' => 'talk', 'sunValue' =>  $sunday, 'monValue' =>  $monday, 'tuesValue' =>  $tuesday, 'wednesValue' =>  $wednesday, 'thursValue' =>  $thursday, 'friValue' =>  $friday, 'saturValue' =>  $saturday);
         } 
         else {
             $response = array('status' => 'fail', 'message' => 'not all values are numbers');
